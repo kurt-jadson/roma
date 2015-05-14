8ca561a7bcfdd53ec2017fae6aca1c35f5ded4b0
kurt-jadson <>
2015-05-14 20:54:48 -0300

    Configurações da metanalise e conversor de entidades

--- src/main/java/br/com/smadp/controller/LoginController.java
+++ src/main/java/br/com/smadp/controller/LoginController.java
@@ -64,7 +64,7 @@ public class LoginController implements Serializable {
 		} catch (Exception e) {
 			LOGGER.warning(e.toString());
 		}
-		return "/login";
+		return "/login?faces-redirect=true";
 	}
 
 	@Produces
